001a8be9b88bf359d967b140e285266927d9ba17
Andrii Oleshko <andriioleshko@gmail.com>
2024-01-11 23:25:26 +0200

    Made some changes in the Main.java due to the recommendations

--- src/main/java/arrays/Main.java
+++ src/main/java/arrays/Main.java
@@ -20,11 +20,11 @@ public class Main {
         System.out.println("The sum of all positive numbers is:");
         System.out.println(sumOfArrayElements(numbersToSum, true));
 
-        int[] numbersToFindAverage = {132, 15, 67, 22};
+        int[] numbersToFindAverage = {132, 15, -2, 67, 22};
         System.out.println("\nOriginal array to find an average value is:");
         System.out.println(Arrays.toString(numbersToFindAverage));
         System.out.println("The average value of all numbers is:");
-        System.out.println((float) sumOfArrayElements(numbersToSum, false) / numbersToSum.length);
+        System.out.println((float) sumOfArrayElements(numbersToFindAverage, false) / numbersToFindAverage.length);
 
         ArrayList<String> numbersArrayList = new ArrayList<>(Arrays.asList("One", "Two", "Tree", "Four"));
         System.out.println("\nOriginal ArrayList is:");
@@ -36,8 +36,9 @@ public class Main {
         ArrayList<Integer> newNumbersArrayList = new ArrayList<>(Arrays.asList(3, 9, 11, 18, 20, 22));
         System.out.println("\nOriginal ArrayList is:");
         System.out.println(newNumbersArrayList);
-        removeUnnecessaryElements(newNumbersArrayList);
-        System.out.println("Updated ArrayList (removed all numbers that are divisible by 3) is:");
+        int divider = 3;
+        removeUnnecessaryElements(newNumbersArrayList, divider);
+        System.out.printf("Updated ArrayList (removed all numbers that are divisible by %d) is:\n", divider);
         System.out.println(newNumbersArrayList);
 
         String objMethods = "Here is a method summary for Object in Java:";
@@ -45,12 +46,11 @@ public class Main {
         objMethods += "\n-> notifyAll()\n-> toString()\n-> wait()\n-> wait(long timeout)\n-> wait(long timeout, int nanos)";
         System.out.printf("\n%s", objMethods);
 
-        System.out.println("\n\nArrays in Java have fixed size because they are implemented as a contiguous block of"
-            + " memory and need specified amount of memory to be reserved when it's initialized.\nBecause of"
-            + " that JVM can directly access needed element of array by calculating in the easy form the exact"
-            + " memory address where it's stored (using memory address of the first element, using the size of the"
-            + " declared type of data stored in this array and multiplying by the index of the element).\n"
-            + "It is quicker than the same operation for ArrayList or LinkedList. ");
+        System.out.println("\n\n");
+        System.out.println("""
+                Arrays in Java have fixed size because they are implemented as a contiguous block of memory and need specified amount of memory to be reserved when it's initialized.
+                Because of that JVM can directly access needed element of array by calculating in the easy form the exact memory address where it's stored (using memory address of the first element, using the size of the declared type of data stored in this array and multiplying by the index of the element).
+                It is quicker than the same operation for ArrayList or LinkedList.\s""");
     }
 
     static Integer[] convertArray(int[] oldArray) {
@@ -63,16 +63,14 @@ public class Main {
 
     static int sumOfArrayElements(int[] array, boolean onlyPositives) {
         int result = 0;
-        for (int el : array) {
-            if (onlyPositives && el <= 0) {
-                continue;
+        for (int el : array)
+            if (el > 0 || (el < 0 && !onlyPositives)) {
+                result += el;
             }
-            result += el;
-        }
         return result;
     }
 
-    static void removeUnnecessaryElements(ArrayList<Integer> arrayList) {
-        arrayList.removeIf(el -> el % 3 == 0);
+    static void removeUnnecessaryElements(ArrayList<Integer> arrayList, int divider) {
+        arrayList.removeIf(el -> el % divider == 0);
     }
 }
